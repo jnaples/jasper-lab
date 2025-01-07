7f29f55e832d61a2015863502a66f19eb3d007c3
dojo <jnaples90@gmail.com>
2025-01-07 07:40:43 -0500

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars2Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
+
+export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-white">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex items-center justify-between gap-x-6 p-5 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img
+              alt=""
+              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+              className="h-8 w-auto"
+            />
+          </a>
+        </div>
+
+        <div className="flex flex-1 items-center justify-end gap-x-6">
+          <a
+            href="#"
+            className="rounded-full bg-slate-900 px-5 py-2  text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-base"
+          >
+            Let's talk
+          </a>
+        </div>
+        <div className="flex">
+          <button
+            type="button"
+            onClick={() => setMenuOpen(true)}
+            className="-m-2.5 inline-flex text-base items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars2Icon aria-hidden="true" className="size-6 mr-2" />
+            Menu
+          </button>
+        </div>
+      </nav>
+      <Dialog open={menuOpen} onClose={setMenuOpen}>
+        <div className="fixed inset-0 z-10 bg-black/25" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-5 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex justify-end items-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-full bg-slate-900 px-5 py-2  text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-base"
+            >
+              Let's talk
+            </a>
+            <button
+              type="button"
+              onClick={() => setMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7  text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+  );
+}
