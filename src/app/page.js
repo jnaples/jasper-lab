a335a4d51ba77782c60bd8af4587f2194fd139fe
dojo <jnaples90@gmail.com>
2025-01-10 14:57:45 -0500

    Change nav open transition

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,38 @@
+"use client";
+import { useEffect, useState } from "react";
 import Hero from "@/components/Hero";
 import NavBar from "@/components/NavBar";
 
 export default function Home() {
+  const [contactForm, setContactForm] = useState(false);
+
+  useEffect(() => {
+    // Check if the Typeform embed script is already loaded
+    if (
+      !document.querySelector(
+        "script[src='//embed.typeform.com/next/embed.js']"
+      )
+    ) {
+      const script = document.createElement("script");
+      script.src = "//embed.typeform.com/next/embed.js";
+      script.async = true;
+      document.body.appendChild(script);
+
+      script.onload = () => {
+        console.log("Typeform script loaded");
+        window.typeformEmbed?.refresh(); // Refresh Typeform embeds
+      };
+    } else {
+      // If the script is already loaded, refresh the embed
+      window.typeformEmbed?.refresh();
+    }
+  }, []);
+
   return (
     <div className="min-h-screen font-[family-name:var(--font-geist-sans)] p-4 lg:px-6">
-      <NavBar />
+      <NavBar setContactForm={setContactForm} />
       <Hero />
+      {contactForm && <div data-tf-live="01J9VQEFKDY0DQP3W9W4JY3M73"></div>}
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Dialog, DialogPanel } from "@headlessui/react";
+import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 import { Bars2Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
@@ -11,7 +11,7 @@ const navigation = [
   { name: "Company", href: "#" },
 ];
 
-export default function NavBar() {
+export default function NavBar({ setContactForm }) {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
@@ -21,15 +21,15 @@ export default function NavBar() {
         className="mx-auto flex items-center justify-between gap-x-6 mb-10"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="">
+          <a href="/" className="">
             <img alt="logo" src="/logo.png" className="w-36" />
           </a>
         </div>
 
         <div className="flex flex-1 items-center justify-end gap-x-6">
           <a
-            href="#"
-            className="rounded-full bg-slate-900 px-5 py-2 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-base transition-all duration-300 ease-in-out"
+            onClick={() => setContactForm(true)}
+            className="rounded-full bg-slate-900 px-5 py-2 text-white shadow-sm hover:bg-indigo-500  text-base transition-all duration-300 ease-in-out cursor-pointer"
           >
             Let's talk
           </a>
@@ -46,9 +46,17 @@ export default function NavBar() {
           </button>
         </div>
       </nav>
+
       <Dialog open={menuOpen} onClose={setMenuOpen}>
-        <div className="fixed inset-0 z-10 bg-black/25" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-5 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogBackdrop
+          className="fixed inset-0 z-10 bg-black/25 backdrop-blur-sm"
+          transition
+        />
+
+        <DialogPanel
+          className="fixed top-0 rounded-bl-2xl right-0 z-10 w-full overflow-y-auto bg-white pl-10 pr-6 py-5 sm:max-w-sm duration-300 data-[closed]:opacity-0 ease-in-out"
+          transition
+        >
           <div className="flex justify-end items-center gap-x-6">
             <a
               href="#"
@@ -67,12 +75,12 @@ export default function NavBar() {
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
+              <div className="space-y-6 pt-10 pb-6">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7  text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-4 text-gray-900 hover:bg-gray-50 text-3xl/7 font-light"
                   >
                     {item.name}
                   </a>
@@ -83,7 +91,7 @@ export default function NavBar() {
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 text-gray-900 hover:bg-gray-50"
                 >
-                  Log in
+                  Instagram
                 </a>
               </div>
             </div>
