b371913e72fb90ac21e804178cc48b156489f341
dojo <jnaples90@gmail.com>
2025-01-14 06:36:27 -0500

    Make font in hero mobile larger

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ export default function Hero() {
 
         <div className="relative h-[500px] rounded-2xl">
           <div className="absolute right-0 top-[-1px] z-10 bg-white md:rounded-bl-2xl">
-            <p className="max-w-[500px] px-4 py-8 font-light md:p-10 lg:text-2xl">
+            <p className="max-w-[500px] px-4 py-8 text-lg font-light md:p-10 lg:text-2xl">
               For trailblazing brands and ambitious visionaries, we don’t just
               design websites – we craft digital experiences that amplify your
               impact and success.
@@ -22,13 +22,12 @@ export default function Hero() {
             <RoundCorner className="-bottom-4 h-4 w-4 scale-x-[-1] will-change-transform md:-left-4 md:bottom-0 md:top-0 md:rotate-90" />
             <RoundCorner className="-bottom-4 right-0 will-change-transform" />
           </div>
-
           <div className="absolute bottom-0 left-0 z-10 rounded-tr-2xl bg-white">
             <div className="h-14 w-20"></div>
             <RoundCorner className="-top-4 left-0 h-4 w-4 rotate-180 will-change-transform" />
             <RoundCorner className="-bottom-0 -right-4 rotate-180 will-change-transform" />
           </div>
-
+          ≈
           <video
             src="/hero-video.mp4"
             alt="hero video of geometric object"
