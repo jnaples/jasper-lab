c2c415e40a9fbfd706e41ab67fbe5f5d598a904d
dojo <jnaples90@gmail.com>
2025-07-03 07:30:43 -0400

    change input colors

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -17,7 +17,7 @@ export default function Input({
       className={clsx(
         "mb-2 mt-2 block w-full rounded-md bg-transparent px-3 py-1.5 text-base outline outline-1 -outline-offset-1 placeholder:text-gray-600 autofill:bg-yellow-200 focus:bg-transparent focus:outline focus:outline-2 focus:-outline-offset-2",
         {
-          "bg-transparent outline-gray-600 focus:outline-gray-600": !error,
+          "bg-transparent outline-gray-400 focus:outline-gray-600": !error,
           "text-red-600 outline-red-300 placeholder:text-red-300 focus:outline-red-600":
             error,
         },
--- src/components/ui/TextArea.jsx
+++ src/components/ui/TextArea.jsx
@@ -20,7 +20,7 @@ export default function TextArea({
           className={clsx(
             "mb-2 block w-full rounded-md bg-transparent px-3 py-1.5 text-base outline outline-1 -outline-offset-1 placeholder:text-gray-600 focus:bg-transparent focus:outline focus:outline-2 focus:-outline-offset-2",
             {
-              "outline-gray-600 focus:outline-gray-600": !error,
+              "outline-gray-400 focus:outline-gray-600": !error,
               "text-red-600 outline-red-300 placeholder:text-red-300 focus:outline-red-600":
                 error,
             },
